8b3b51d18bea4d39c94ef8568af2b3a052524c81
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-04-15 20:33:27 +0200

    update styles in index.css for improved design and add button styles; import CSS in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {RouterProvider} from "react-router-dom"
 import {router} from './router.tsx'
 import {QueryClientProvider} from "@tanstack/react-query"
 import {queryClient} from "./queryClient.ts";
+import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
