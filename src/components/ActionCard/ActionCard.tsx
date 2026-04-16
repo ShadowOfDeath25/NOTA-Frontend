026b49f1038246726136d87509709db679450176
Your Name <you@example.com>
2026-04-16 20:43:06 +0200

    add ActionList & ActionCard component With RTL support ; update translate file

---
+++ src/components/ActionCard/ActionCard.tsx
@@ -0,0 +1,17 @@
+import styles from './ActionCard.module.css';
+
+const ActionCard = ({ title, description, icon, iconColorClass }) => {
+  return (  
+    <div className={`${styles.card}`}>
+      <div className={`${styles.iconContainer} ${styles[iconColorClass]}`}>
+      <img src={icon} alt="add icon" />  
+      </div>
+      <div className={styles.textContainer}>
+        <h3 className={`${styles.title} bodyText`}>{title}</h3>
+        <p className={`${styles.description} bodyTextSm`}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ActionCard;
