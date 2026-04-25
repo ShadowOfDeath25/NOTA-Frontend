5537ac0465c46fb835e97bb718c12de72145d62c
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-04-25 17:23:32 +0300

    Refactor: optimized AxiosClient for API versioning

--- src/axiosClient.ts
+++ src/axiosClient.ts
@@ -1,13 +1,14 @@
 import axios from 'axios'
+import type {AxiosInstance} from 'axios'
 
-const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+
+export const AxiosClientV1: AxiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/${import.meta.env.VITE_API_VERSION}`,
     withCredentials: true,
     withXSRFToken: true,
     headers: {
         Accept: "application/json"
-    }
+    },
 })
 
 
-export default axiosClient
