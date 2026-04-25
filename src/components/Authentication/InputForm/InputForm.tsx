67f9cf7978e510edda95a248103349823c81e0ea
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-04-25 20:01:52 +0300

    update InputForm component to support helper text and error states; refactor CSS module for improved styling and layout logic

--- src/components/Authentication/InputForm/InputForm.tsx
+++ src/components/Authentication/InputForm/InputForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./InputForm.module.css";
 // @ts-ignore
 import EyeIcon from "@assets/icons/eye.svg?react";
@@ -6,51 +6,69 @@ import EyeIcon from "@assets/icons/eye.svg?react";
 import EyeOffIcon from "@assets/icons/eyeoff.svg?react";
 
 interface InputFieldProps {
-  label: string;
-  type?: string;
-  placeholder?: string;
-  id: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    label: string;
+    type?: string;
+    placeholder?: string;
+    id: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    helperText?: string;
+    isError?: boolean;
 }
 
 
-export const InputForm = ({ label, type = "text", placeholder, id, value, onChange }: InputFieldProps) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const isPassword = type === "password";
+export const InputForm = ({
+                              label,
+                              type = "text",
+                              placeholder,
+                              id,
+                              value,
+                              onChange,
+                              helperText,
+                              isError
+                          }: InputFieldProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const isPassword = type === "password";
 
 
-  const inputType = isPassword ? (showPassword ? "text" : "password") : type;
- 
-  return (
-    <div className={styles.emailInputContainer}>
-      <label htmlFor={id} className={`${styles.label} bodyTextSm`}>
-        {label}
-      </label>
-      
-      <div className={isPassword ? styles.passwordContainer : ""}>
-        <div className={styles.passwordInputContainer}>
-          <input
-            id={id}
-            type={inputType}
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            className={`${styles.input} bodyTextSm`}
-          />
+    const inputType = isPassword ? (showPassword ? "text" : "password") : type;
+    const inputClassName = `${styles.input} bodyTextSm ${isError ? styles.inputError : ""}`;
+
+    return (
+        <div className={styles.container}>
+            <label htmlFor={id} className={`${styles.label} bodyTextSm`}>
+                {label}
+            </label>
+
+            <div className={isPassword ? styles.passwordContainer : ""}>
+                <div className={`${styles.inputContainer}`}>
+                    <input
+                        id={id}
+                        type={inputType}
+                        placeholder={placeholder}
+                        value={value}
+                        onChange={onChange}
+                        className={inputClassName}
+                    />
+                </div>
+
+                {isPassword && (
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className={styles.passwordToggle}
+                    >
+
+                        {showPassword ? <EyeOffIcon className={styles.icon}/> : <EyeIcon className={styles.icon}/>}
+                    </button>
+                )}
+                {helperText && (
+                    <span className={`${styles.helperText} ${isError ? styles.helperTextError : ""}`}>
+                        {helperText}
+                    </span>
+                )}
+            </div>
+
         </div>
-
-        {isPassword && (
-          <button
-            type="button"
-            onClick={() => setShowPassword(!showPassword)}
-            className={styles.passwordToggle}
-          >
-           
-            {showPassword ? <EyeOffIcon className={styles.icon}/> : <EyeIcon className={styles.icon}/>}
-          </button>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
