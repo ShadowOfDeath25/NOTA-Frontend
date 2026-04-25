78a0c6e7434384a0e518ad221be86bc35e4b7ba7
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-04-25 15:46:32 +0300

    Split routes based on domain and added alias for /src/routes path

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,10 @@
 import App from "./App.tsx";
 import {createBrowserRouter} from "react-router-dom";
 import type {RouteObject} from 'react-router'
-import LoginPage from "./pages/auth/LoginPage/LoginPage.tsx";
-import SignupPage from "./pages/auth/SignupPage/SignupPage.tsx";
-import ResetPasswordPage from "./pages/auth/ResetPasswordPage/RestPasswordPage.tsx";
-import NewPasswordPage from "./pages/auth/NewPasswordPage/NewPasswordPage.tsx";
+import authRoutes from "@routes/auth";
+
 export const routes: RouteObject[] = [
-    {
-        path: "/",
-        element: <LoginPage/>
-    },
+    ...authRoutes,
     {
         path: "/home",
         element: <App/>
@@ -34,22 +29,7 @@ export const routes: RouteObject[] = [
         path: "trash",
         element: <App/>
     },
-    {
-        path: "/login",
-        element: <LoginPage/>
-    },
-    {
-        path: "/signup",
-        element: <SignupPage/>
-    },
-    {
-        path: "/forgot-password",
-        element: <ResetPasswordPage/>
-    },
-    {
-        path: "/new-password",
-        element: <NewPasswordPage/>
-    }
+
 ]
 
 export const router = createBrowserRouter(routes);
---
+++ src/routes/auth.tsx
@@ -0,0 +1,27 @@
+import {routes} from "../router.tsx";
+import type {RouteObject} from "react-router";
+import LoginPage from "@pages/auth/LoginPage/LoginPage.tsx";
+import SignupPage from "@pages/auth/SignupPage/SignupPage.tsx";
+import ResetPasswordPage from "@pages/auth/ResetPasswordPage/RestPasswordPage.tsx";
+import NewPasswordPage from "@pages/auth/NewPasswordPage/NewPasswordPage.tsx";
+
+const routes: RouteObject[] = [
+    {
+        path: "/login",
+        element: <LoginPage/>
+    },
+    {
+        path: "/signup",
+        element: <SignupPage/>
+    },
+    {
+        path: "/forgot-password",
+        element: <ResetPasswordPage/>
+    },
+    {
+        path: "/new-password",
+        element: <NewPasswordPage/>
+    }
+]
+
+export default routes;
