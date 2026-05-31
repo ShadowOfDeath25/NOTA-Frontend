27035cc2673d0c3dc1f0ca9ce7617cb6eb812f89
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-05-31 16:51:03 +0300

    memoize currentUser in Editor, add /ws proxy to Vite config, and refactor useCollaboration provider management using refs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
@@ -12,13 +12,17 @@ export default defineConfig(({ mode }) => {
 
         server: {
             allowedHosts: true,
-
             proxy: {
                 '/api': {
                     target: env.VITE_API_BASE_URL,
                     changeOrigin: true,
                     secure: false,
                 },
+                "/ws": {
+                    target: env.VITE_WS_PROVIDER_URL,
+                    changeOrigin: true,
+                    secure: false
+                }
             },
         },
     }
