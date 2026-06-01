71b030ec5d7338b023d673e1991fff3c7b6a09ec
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-06-01 16:36:59 +0300

    Changed the note-ID for testing purposes

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,6 +1,6 @@
 import Editor from "@components/Editor/Editor.tsx";
 
 export default function Test() {
-    return <Editor noteId="019e2d09-8c32-727b-93a4-62e9a977ade2"/>;
+    return <Editor noteId="019e7df2-347c-729a-99a2-1ce96ed434fc"/>;
 }
 
