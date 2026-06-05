64266d0c1621f5bc0061e7add8346b02cffa5f9d
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-06-05 18:15:53 +0300

    configure Laravel Echo with Reverb broadcaster and add Pusher dependency

---
+++ src/echo.ts
@@ -0,0 +1,21 @@
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+
+// @ts-expect-error This object is added to the window object
+window.Pusher = Pusher
+
+export const echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: Number(import.meta.env.VITE_REVERB_PORT),
+    forceTLS: false,
+    enabledTransports: ['ws', 'wss'],
+    authEndpoint: '/api/broadcasting/auth',
+    withCredentials: true,
+    auth: {
+        headers: {
+            accept: "application/json"
+        }
+    }
+})
